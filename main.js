9260a6028376470bf53d99121ecd590cf389e33b
mdshafii786 <mdshafiii959@gmail.com>
2025-09-16 14:51:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,19 +4,3 @@ $(document).ready(function () {
 })
 
 // Scroll animation effect
-$(document).ready(function () {
-  function revealOnScroll() {
-    $(".reveal").each(function () {
-      var windowBottom = $(window).scrollTop() + $(window).height();
-      var elementTop = $(this).offset().top;
-
-      if (elementTop < windowBottom - 50) {
-        $(this).addClass("animate");
-      }
-    });
-  }
-
-  // Run on scroll + load
-  $(window).on("scroll", revealOnScroll);
-  revealOnScroll();
-});
