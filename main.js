934bf6224102b24a6a11cff2052f33881bf8c05b
mdshafii786 <mdshafiii959@gmail.com>
2025-09-16 14:23:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
   console.log("Thumbay Publicity site loaded!");
   // You can add interactive scripts here later
+})
+
+// Scroll animation effect
+$(document).ready(function () {
+  function revealOnScroll() {
+    $(".reveal").each(function () {
+      var windowBottom = $(window).scrollTop() + $(window).height();
+      var elementTop = $(this).offset().top;
+
+      if (elementTop < windowBottom - 50) {
+        $(this).addClass("animate");
+      }
+    });
+  }
+
+  // Run on scroll + load
+  $(window).on("scroll", revealOnScroll);
+  revealOnScroll();
 });
